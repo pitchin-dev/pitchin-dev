f850f19bcffd6480cc512f8f3b976fe03420c373
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-24 18:27:04 +0300

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,15 +15,15 @@ const Header = () => {
                         <h1 className="name">Pavel Itchin</h1>
                         <h2 className="desc">Junior Frontend Developer</h2>   
                         <ul className="social list-inline">
-                            <li className="list-inline-item"><a href="https://vk.com/itchin_pavel" target="_blank"><i className="fa fa-vk"></i></a></li>                   
-                            <li className="list-inline-item"><a href="https://www.instagram.com/itchin_pavel/" target="_blank"><i className="fa fa-instagram"></i></a></li>
-                            <li className="list-inline-item"><a href="https://twitter.com/pitchin_dev" target="_blank"><i className="fa fa-twitter"></i></a></li>
-                            <li className="list-inline-item"><a href="https://www.facebook.com/pavel.itchin" target="_blank"><i className="fa fa-facebook"></i></a></li>                  
-                            <li className="list-inline-item last-item"><a href="https://github.com/pitchin-dev" target="_blank"><i className="fa fa-github"></i></a></li>                 
+                            <li className="list-inline-item"><a href="https://vk.com/itchin_pavel" target="_blank" rel="noopener noreferrer"><i className="fa fa-vk"></i></a></li>                   
+                            <li className="list-inline-item"><a href="https://www.instagram.com/itchin_pavel/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
+                            <li className="list-inline-item"><a href="https://twitter.com/pitchin_dev" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a></li>
+                            <li className="list-inline-item"><a href="https://www.facebook.com/pavel.itchin" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>                  
+                            <li className="list-inline-item last-item"><a href="https://github.com/pitchin-dev" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a></li>                 
                         </ul> 
                     </div>
                     <div className="col-md-2">
-                        <a href="https://t.me/itchin_pavel" target="_blank"><button className="btn btn-primary"><i className="fa fa-paper-plane"></i>Contact Me</button></a>
+                        <a href="https://t.me/itchin_pavel" target="_blank" rel="noopener noreferrer"><button className="btn btn-primary"><i className="fa fa-paper-plane"></i>Contact Me</button></a>
                     </div>             
                 </div>
             </div>         

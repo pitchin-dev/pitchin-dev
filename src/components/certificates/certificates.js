bd5314765c7838f7772583b5423a5c98f954364d
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-24 18:26:59 +0300

    Update certificates.js

--- src/components/certificates/certificates.js
+++ src/components/certificates/certificates.js
@@ -9,18 +9,18 @@ const Certificates = () => {
                 <h2 className="heading">My Certificates</h2>
                 <div className="content">
                     <div className="item">
-                        <a href="https://www.udemy.com/certificate/UC-2a9a2e70-cbf4-48b6-a258-976676052827/" target="_blank">
+                        <a href="https://www.udemy.com/certificate/UC-2a9a2e70-cbf4-48b6-a258-976676052827/" target="_blank" rel="noopener noreferrer">
                             <img src={process.env.PUBLIC_URL + 'certificate-1.jpg'} alt="" className="img-fluid"/>
                         </a>
                     </div>
                     <div className="item">
-                    <a href="https://www.udemy.com/certificate/UC-3b7346ff-09de-4853-bd3c-53fbd64179ef/" target="_blank">
+                    <a href="https://www.udemy.com/certificate/UC-3b7346ff-09de-4853-bd3c-53fbd64179ef/" target="_blank" rel="noopener noreferrer">
                             <img src={process.env.PUBLIC_URL + 'certificate-2.jpg'} alt="" className="img-fluid"/>
                         </a>
                     </div>
                     
                     <div className="item">
-                    <a href="https://www.udemy.com/certificate/UC-57e4cd4c-c618-47e2-b0a0-9681a5552a42/" target="_blank">
+                    <a href="https://www.udemy.com/certificate/UC-57e4cd4c-c618-47e2-b0a0-9681a5552a42/" target="_blank" rel="noopener noreferrer">
                             <img src={process.env.PUBLIC_URL + 'certificate-3.jpg'} alt="" className="img-fluid"/>
                         </a>
                     </div>

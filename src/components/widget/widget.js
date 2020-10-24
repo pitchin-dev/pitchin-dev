bfecda525942ff2981de58a20e5b448d8c438978
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-24 23:04:09 +0300

    Create widget.js

---
+++ src/components/widget/widget.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './widget.css';
+
+const Widget = () => {
+    return (
+        <aside className="widget aside section">
+            <div className="section-inner">
+                <div className="content">
+                    <div className="elfsight-app-7b43aa13-b592-4714-a4f8-7fd7ca2dbdf4"></div>
+                </div>
+            </div>
+        </aside>
+    )
+}
+
+export {Widget};

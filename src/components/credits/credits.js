80068793205514d955cbc48dbdf4e5b14f447eb8
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-24 18:27:02 +0300

    Update credits.js

--- src/components/credits/credits.js
+++ src/components/credits/credits.js
@@ -10,12 +10,12 @@ const Credits = () => {
                 <div className="content">
                     <p>This web page was made using ReactJS, Bootstrap 4 and Font Awesome sprites.</p>
                     <ul className="list-unstyled">
-                        <li><a href="http://getbootstrap.com/" target="_blank"><i className="fa fa-external-link"></i> Bootstrap</a></li>
-                        <li><a href="http://fortawesome.github.io/Font-Awesome/" target="_blank"><i className="fa fa-external-link"></i> FontAwesome</a></li>
+                        <li><a href="http://getbootstrap.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link"></i> Bootstrap</a></li>
+                        <li><a href="http://fortawesome.github.io/Font-Awesome/" target="_blank" rel="noopener noreferrer"><i className="fa fa-external-link"></i> FontAwesome</a></li>
                         <li>Profile image made by me.</li>
                     </ul>                
                     <hr/>                 
-                        <p>This responsive portfolio template is handcrafted by UX designer <a href="https://www.linkedin.com/in/xiaoying" target="_blank">Xiaoying Riley</a> at <a href="http://themes.3rdwavemedia.com/" target="_blank">3rd Wave Media</a></p>
+                        <p>This responsive portfolio template is handcrafted by UX designer <a href="https://www.linkedin.com/in/xiaoying" target="_blank" rel="noopener noreferrer">Xiaoying Riley</a> at <a href="http://themes.3rdwavemedia.com/" target="_blank" rel="noopener noreferrer">3rd Wave Media</a></p>
                 </div>
             </div>
         </aside>

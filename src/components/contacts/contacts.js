736b0c49e7662ea4fcba353675f3c3f647c916c6
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-25 20:09:27 +0300

    Update contacts.js

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -11,7 +11,7 @@ const Contacts = () => {
                     <ul className="list-unstyled">
                         <li><i className="fa fa-map-marker mr-2"></i><span className="sr-only">Location:</span>Moscow, Russia</li>
                         <li><i className="fa fa-envelope-o mr-2"></i><span className="sr-only">Email:</span><a href="mailto:itchin.pavel@yandex.ru">itchin.pavel@yandex.ru</a></li>
-                        <li><i className="fa fa-link mr-2"></i><span className="sr-only">Website:</span><a href="http://www.pitchin-dev.ru">http://www.pitchin-dev.com</a></li>
+                        <li><i className="fa fa-link mr-2"></i><span className="sr-only">Website:</span><a href="http://www.pitchin-dev.ru">http://www.pitchin-dev.ru</a></li>
                         <li><i className="fas fa-mobile-alt mr-2"></i><span className="sr-only">Phone number:</span><a href="tel:+79259062231">+79259062231</a></li>
                     </ul>
                 </div>                    

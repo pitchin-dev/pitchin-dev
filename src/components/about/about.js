b058f43526ee2a5c04e0e3ca2e770e2aab86c33a
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-25 19:54:52 +0300

    Update about.js

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,19 +10,25 @@ const About = () => {
                 <div className="content">
 
                     <p>
-                        Hello, let me introduce myself. My name is Pavel, I'm 28 years old. 
-                        Living my whole life in Moscow. This is the city that I love so much.
+                        Dear visitor, welcome to my personal website. Let me speak some words about myself. My name is Pavel, I'm 28 years old, living my whole life in Moscow. 
                     </p>
 
                     <p>
-                        I have a dream to become frontend developer.
+                        I have a dream to become frontend developer and I consistently, step by step making my moves to improve my skills to get my first real job in frontend!
                     </p>
+
+                    <p>I have a bachelor's degree in law (Moscow Financial and Law Academy), 2020 graduated. But in process of studying I realised what I really want to do. And this is the frontend development.</p>
     
                     <p>
                         My personal interests and hobbies, besides frontend development is rock music, 
-                        European football and urbanistics. Love to discover new places in my adorable city of Moscow 
-                        and aknowledge new things about the places that I already know.
+                        European football, fantasy and science-fiction movies, especially Star Wars saga, which I may call the love of my whole life :)
                     </p>
+
+                    <p>
+                        Love to discover new places in my adorable city of Moscow and aknowledge new things about the places that I already know.
+                    </p>
+
+                    <p>My personal qualities are: organized, accurate, attentive, quick learner, loyal, motivated and improvement-oriented.</p>
                     
                 </div>
             </div>                
